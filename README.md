c5e305db068e1aaaae498911cf06533217dd3413
Rounak Sen <rony000013@gmail.com>
2023-02-03 13:33:15 +0530

    created project

---
+++ README.md
@@ -0,0 +1,28 @@
+# &nbsp;&nbsp;&nbsp;&nbsp;📺 Youtube Video Downloader 720p
+
+usage in Comand Prompt: 
+```
+python3 youtube_video_downloader_720p.py [link]
+```
+<br>
+
+### It is a video downloader for YouTube created using pytube and some other libraries for downloading 720p videos in mp4 format.
+
+<br>
+
+> Insert link in place of **[link]** in comand prompt.
+
+> It can be used to download YouTube videos of only 720p size.
+
+> pytube is only dependency required.
+
+> To install pytube ->
+
+    pip install pytube
+
+> To Batch install videos insert all links in links list
+
+<br>
+<br>
+
+created by Rounak Sen
---
+++ youtube_downloader_720p.py
@@ -0,0 +1,64 @@
+from pytube import YouTube
+from sys import argv
+import os
+from pathlib import Path
+
+
+'''
+        📺  Youtube Video Downloader 720p
+
+    usage in Comand Prompt: python3 youtube_video_downloader_720p.py [link]
+
+    It is a video downloader for YouTube created using pytube and some other libraries for downloading 720p videos in mp4 format.
+    
+    > Insert link in place of [link] in comand prompt.
+    > It can be used to download YouTube videos of only 720p size.
+    > pytube is only dependency required.
+    > To install pytube -> pip install pytube
+    > To Batch install videos insert all links in links list
+
+'''
+
+# For Batch download insert all links in string type in list below.
+links = []
+
+# Checking if link is provided
+try:
+    link = argv[1]
+except IndexError:
+    link = None
+
+# To change download location fell free to change below
+path_to_download_folder = str(os.path.join(Path.home(), "Downloads"))
+
+
+# YouTube video download Function
+def Youtube_Downloader(Link):
+
+    # Checking if link exists
+    if link is None:
+        print("😞 No link was provided so no video downloaded.")
+        return
+
+    # youtube object
+    yt = YouTube(link)
+
+    print("Title = ", yt.title)
+    print("Downloading... ⏳⌛⏳⌛")
+
+    # Selects the 720p version for video
+    yd = yt.streams.get_highest_resolution()
+
+    # Downloads the video
+    yd.download(path_to_download_folder)
+
+    print("🙂 Completed download. Enjoy. 😎")
+
+
+# Calling the Youtube_Downloader function
+Youtube_Downloader(link)
+
+# Batch downloading videos
+if links != []:
+    for link in links:
+        YouTube_Downloader(link)
